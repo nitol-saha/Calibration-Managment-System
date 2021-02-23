82fe0551de8665758fbf4f31c483f082b5e5409a
nitol-saha <63906863+nitol-saha@users.noreply.github.com>
2021-02-23 19:01:17 +0600

    90% complete

--- src/CMS/StandardScheduleController.java
+++ src/CMS/StandardScheduleController.java
@@ -9,9 +9,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -35,11 +34,39 @@ public class StandardScheduleController implements Initializable {
 
     @FXML
     private ChoiceBox frequency_checkbox;
+    @FXML
+    private TableColumn col_sd_id;
+    @FXML
+    private TableColumn col_jan;
+    @FXML
+    private TableColumn col_feb;
+    @FXML
+    private TableColumn col_mar;
+    @FXML
+    private TableColumn col_apr;
+    @FXML
+    private TableColumn col_may;
+    @FXML
+    private TableColumn col_jun;
+    @FXML
+    private TableColumn col_jul;
+    @FXML
+    private TableColumn col_aug;
+    @FXML
+    private TableColumn col_sep;
+    @FXML
+    private TableColumn col_oct;
+    @FXML
+    private TableColumn col_nov;
+    @FXML
+    private TableColumn col_dec;
+    @FXML
+    private TableView sche_table;
 
 
     public void MenuWindow(ActionEvent event) throws IOException {
-        ((Node)event.getSource()).getScene().getWindow().hide();
-        Stage primaryStage= new Stage();
+        ((Node) event.getSource()).getScene().getWindow().hide();
+        Stage primaryStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
         primaryStage.setTitle("Menu");
         primaryStage.setScene(new Scene(root));
@@ -51,17 +78,34 @@ public class StandardScheduleController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
 
+        query = new StandardScheduleDbQuery();
 
-        query= new StandardScheduleDbQuery();
+        col_sd_id.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("sd_id"));
+        col_jan.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("jan"));
+        col_feb.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("feb"));
+        col_mar.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("mar"));
+        col_apr.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("apr"));
+        col_may.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("may"));
+        col_jun.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("jun"));
+        col_jul.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("jul"));
+        col_aug.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("aug"));
+        col_sep.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("sep"));
+        col_oct.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("oct"));
+        col_nov.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("nov"));
+        col_dec.setCellValueFactory(new PropertyValueFactory<StandardScheduleModel, String>("dec"));
+
+
         DateFormatSymbols dfs = new DateFormatSymbols();
         String[] months_temp = dfs.getMonths();
-        String[] months= Arrays.copyOf(months_temp, months_temp.length-1);
+        String[] months = Arrays.copyOf(months_temp, months_temp.length - 1);
         List<String> monthList = Arrays.asList(months);
         ObservableList<String> MonthList = FXCollections.observableList(monthList);
-        ObservableList<String> WeekList = FXCollections.observableArrayList("1st Week","2nd Week","3d Week","4th Week");
-        ObservableList<String> FrequencyList = FXCollections.observableArrayList("1","3","6","12");
+        ObservableList<String> WeekList = FXCollections.observableArrayList("1st", "2nd", "3rd", "4th");
+        ObservableList<String> FrequencyList = FXCollections.observableArrayList("1", "3", "6", "12");
         try {
-            sd_id_choicebox.setItems(query.get_sd_id_list());;
+            sche_table.setItems(query.get_schedule_list());
+            sd_id_choicebox.setItems(query.get_sd_id_list());
+            ;
             month_choicebox.setItems(MonthList);
             week_choicebox.setItems(WeekList);
             frequency_checkbox.setItems(FrequencyList);
@@ -73,34 +117,55 @@ public class StandardScheduleController implements Initializable {
     }
 
 
-    public void add_sd_schedule (ActionEvent event){
+    public void add_sd_schedule(ActionEvent event) {
         int frequency = Integer.parseInt(String.valueOf(frequency_checkbox.getValue()));
-        int loop_count= 12/frequency;
+        int loop_count = 12 / frequency;
         DateFormatSymbols dfs = new DateFormatSymbols();
         String[] months_temp = dfs.getMonths();
-        String[] months= Arrays.copyOf(months_temp, months_temp.length-1);
+        String[] months = Arrays.copyOf(months_temp, months_temp.length - 1);
         List<String> monthList = Arrays.asList(months);
         int month_position = monthList.indexOf(month_choicebox.getValue());
-        query= new StandardScheduleDbQuery();
-        for (int i = 0; i < loop_count; i++)
-            {
+        query = new StandardScheduleDbQuery();
+
+        for (int i = 0; i < loop_count; i++) {
 
 
-                StandardScheduleModel model = new StandardScheduleModel(sd_id_choicebox.getValue().toString(), monthList.get(month_position), week_choicebox.getValue().toString());
+            StandardScheduleInsertModel model = new StandardScheduleInsertModel(sd_id_choicebox.getValue().toString(), monthList.get(month_position), week_choicebox.getValue().toString());
+            if(i==0) {
                 int count = query.add_sd_schedule(model);
-                System.out.printf("\nID : "+sd_id_choicebox.getValue().toString() );
-                System.out.println("\nMonth :" + monthList.get(month_position));
-                System.out.println("Week :" +week_choicebox.getValue().toString());
-                month_position=month_position+frequency;
-                if(month_position>12)
-                {
-                    month_position=month_position-12;
+            }
+            else {
 
-                }
+                int count = query.update_sd_schedule(model);
 
             }
+
+            System.out.printf("\nID : " + sd_id_choicebox.getValue().toString());
+            System.out.println("\nMonth :" + monthList.get(month_position));
+            System.out.println("Week :" + week_choicebox.getValue().toString());
+            month_position = month_position + frequency;
+            if (month_position > 11) {
+                month_position = month_position - 12;
+
+            }
+
+        }
+        refreshTable();
 
 
     }
 
+    private void refreshTable() {
+        try {
+            sche_table.setItems(query.get_schedule_list());
+
+
+
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+
+    }
 }
